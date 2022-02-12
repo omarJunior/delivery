c619e8c35405bffcd93aaa50c8b4b03e7a976a03
Omar junior <omarhernandez041204@gmail.com>
2022-02-12 15:08:41 -0500

    Cambios en el modelo de persona

--- appPersona/models.py
+++ appPersona/models.py
@@ -1,20 +1,20 @@
 from django.db import models
 from appConfiguracion.models import TipoIndetificacion
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 
 # Create your models here.
 class Cliente(models.Model):
-    codigo = models.CharField(max_length=55, null=True, blank=True)
-    tipoIdentificacion = models.ForeignKey(TipoIndetificacion, null=True, blank=True, on_delete=models.SET_NULL)
-    numeroIdentificacion = models.CharField(max_length=60, null=True, blank=True)
-    nombres = models.CharField(max_length=100, null=True, blank=True)
-    apellidos = models.CharField(max_length=100, null=True, blank=True)
-    correo_electronico = models.EmailField(null=True, blank=True, unique=True)
-    fecha_nacimiento = models.DateField(null=True, blank=True)
-    edad = models.IntegerField(null=True, blank=True)
-    direccion = models.CharField(max_length=50, null=True, blank=True)
-    telefono = models.CharField(max_length=50, null=True, blank=True)
-    descuento = models.FloatField(null=True, blank=True)
+    codigo = models.CharField(verbose_name="Codigo del cliente", max_length=55, null=True, blank=True)
+    tipoIdentificacion = models.ForeignKey(TipoIndetificacion, verbose_name="Tipo de identificacion", null=True, blank=True, on_delete=models.SET_NULL)
+    numeroIdentificacion = models.CharField("Numero de identificacion", max_length=60, null=True, blank=True)
+    nombres = models.CharField(verbose_name="Nombres", max_length=100, null=True, blank=True)
+    apellidos = models.CharField(verbose_name="Apellidos", max_length=100, null=True, blank=True)
+    correo_electronico = models.EmailField(verbose_name= "Email", null=True, blank=True, unique=True)
+    fecha_nacimiento = models.DateField(verbose_name="Fecha Nacimiento", null=True, blank=True)
+    edad = models.IntegerField(verbose_name="Edad", null=True, blank=True)
+    direccion = models.CharField(verbose_name="Direccion", max_length=50, null=True, blank=True)
+    telefono = models.CharField(verbose_name="Telefono", max_length=50, null=True, blank=True)
+    descuento = models.FloatField(verbose_name="Descuento", null=True, blank=True)
 
     @property
     def get_full_name(self):
@@ -34,19 +34,28 @@ class Cliente(models.Model):
 
 #Hacer referencia al auth user
 class Persona(models.Model):
-    obj_user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
-    tipoIdentificacion = models.ForeignKey(TipoIndetificacion, null=True, blank=True, on_delete=models.SET_NULL)
-    numeroIdentificacion = models.CharField(max_length=60, null=True, blank=True)
-    nombres = models.CharField(max_length=100, null=True, blank=True)
-    apellidos = models.CharField(max_length=100, null=True, blank=True)
-    correo_electronico = models.EmailField(null=True, blank=True, unique=True)
-    edad = models.IntegerField(null=True, blank=True)
-    rol_persona = models.CharField(max_length=60, null=True, blank=True)
+    obj_user = models.ForeignKey(User, verbose_name="Usuario registrado", null=True, blank=True, on_delete=models.SET_NULL)
+    tipoIdentificacion = models.ForeignKey(TipoIndetificacion, verbose_name="Tipo de identificacion", null=True, blank=True, on_delete=models.SET_NULL)
+    numeroIdentificacion = models.CharField(verbose_name="Numero de identificacion", max_length=60, null=True, blank=True)
+    nombres = models.CharField(verbose_name="Nombres", max_length=100, null=True, blank=True)
+    apellidos = models.CharField(verbose_name="Apellidos", max_length=100, null=True, blank=True)
+    correo_electronico = models.EmailField(verbose_name="Email", null=True, blank=True, unique=True)
+    edad = models.IntegerField(verbose_name="Edad", null=True, blank=True)
+    rol_persona = models.ManyToManyField(Group)
     sys_active = models.BooleanField(default=True)
 
     @property
     def get_full_name_persona(self):
         return self.nombres + " " + self.apellidos
+
+    @property
+    def get_rol_persona(self):
+        grupos = ""
+        grupo = self.obj_user.groups.all()
+        if grupo.count() > 0:
+            for i in grupo:
+                grupos  = grupos + i.name
+        return grupos
 
     sys_fechaCreacion = models.DateTimeField(auto_now_add= True)
     sys_fechaActualizacion = models.DateTimeField(auto_now = True)
@@ -57,5 +66,5 @@ class Persona(models.Model):
         verbose_name_plural = "Personas"
 
     def __str__(self):
-        return self.rol_persona + " " + self.get_full_name_persona()
+        return self.get_rol_persona + " " + self.get_full_name_persona
 
