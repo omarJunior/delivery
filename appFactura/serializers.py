898cb46110f7939d3ae0fb6397c1e72c888b73d3
Omar junior <omarhernandez041204@gmail.com>
2022-02-14 11:38:54 -0500

    cambios en el serializers del appFactura

--- appFactura/serializers.py
+++ appFactura/serializers.py
@@ -2,22 +2,64 @@ from rest_framework import serializers
 from .models import Factura, Producto, Receta
 
 class FacturaSerializer(serializers.ModelSerializer):
+    obj_cliente = serializers.SerializerMethodField(method_name="get_full_name")
+    obj_forma_pago = serializers.CharField(source = "obj_forma_pago.nombre")
+
     class Meta:
         model = Factura
-        fields = ('__all__')
-        extra_kwargs = {'id': {'required': True}}
+        fields = [
+            'id',
+            'nro_comprobante',
+            'fecha',
+            'obj_cliente',
+            'obj_forma_pago',
+            'iva',
+            'descuento'
+            
+        ]
+        read_only_fields = ('obj_cliente', 'obj_forma_pago',)
+
+    def get_full_name(self, obj):
+        if obj.obj_cliente:
+            return obj.obj_cliente.get_full_name
+        return ""
+        
 
 class ProductoSerializer(serializers.ModelSerializer):
+    obj_rubro = serializers.CharField(source = "obj_rubro.descripcion")
+    obj_stock = serializers.CharField(source = "obj_stock.stock")
+
     class Meta:
         model = Producto
-        fields = ('__all__')
-        extra_kwargs = {'id': {'required': True}}
-
+        fields = [
+            'id',
+            'descripcion',
+            'costo',
+            'precio_sin_iva',
+            'precio_final',
+            'obj_rubro',
+            'obj_stock',
+            'tamanio'
+        ]
+        read_only_fields = ('obj_rubro', 'obj_stock')
     
 class RecetaSerializer(serializers.ModelSerializer):
+    obj_ingrediente = serializers.CharField(source = "obj_ingrediente.descripcion")
+    obj_producto = serializers.SerializerMethodField(method_name="get_producto")
+    
     class Meta:
         model = Receta
-        fields = ('__all__')
-        extra_kwargs = {'id': {'required': True}}
+        fields = [
+            'id',
+            'obj_ingrediente',
+            'obj_producto'
+        ]
+        read_only_fields = ('obj_ingrediente', 'obj_producto',)
+
+    def get_producto(self, obj):
+        if obj.obj_producto:
+            return obj.obj_producto.descripcion
+        return ""
+
 
     
