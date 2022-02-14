73404fe2d1f551491c8eee100caac1231fadd7f0
Omar junior <omarhernandez041204@gmail.com>
2022-02-14 11:38:05 -0500

    serializer de persona

--- appPersona/serializers.py
+++ appPersona/serializers.py
@@ -2,13 +2,55 @@ from rest_framework import serializers
 from .models import Cliente, Persona
 
 class ClienteSerializer(serializers.ModelSerializer):
+    tipoIdentificacion = serializers.CharField(source = "tipoIdentificacion.descripcion")
+    obj_user_referenciado = serializers.SerializerMethodField(method_name = "get_full_name")
+    
     class Meta:
         model = Cliente
-        fields = ('__all__')
-        extra_kwargs = {'id': {'required': True}}
+        fields = [
+            'id',
+            'codigo',
+            'tipoIdentificacion',
+            'numeroIdentificacion',
+            'nombres',
+            'apellidos',
+            'correo_electronico',
+            'fecha_nacimiento',
+            'edad',
+            'direccion',
+            'telefono',
+            'descuento',
+            'obj_user_referenciado',
+        ]
 
+        read_only_fields = ('tipoIdentificacion', 'obj_user_referenciado',) 
+    
+    def get_full_name(self, obj):
+        if obj.obj_user_referenciado:
+            return obj.obj_user_referenciado.get_full_name()
+        return ""
+        
 class PersonaSerializer(serializers.ModelSerializer):
+    obj_user = serializers.SerializerMethodField(method_name="get_full_name")
+    tipoIdentificacion = serializers.CharField(source = "tipoIdentificacion.descripcion")
+
     class Meta:
         model = Persona
-        fields = ('__all__')
-        extra_kwargs = {'id': {'required': True}}
+        fields = [
+            'id',
+            'obj_user',
+            'tipoIdentificacion',
+            'numeroIdentificacion',
+            'nombres',
+            'apellidos',
+            'correo_electronico',
+            'edad',
+            'get_rol_persona',
+        ]
+        read_only_fields = ('tipoIdentificacion', 'obj_user',) 
+
+    def get_full_name(self, obj):
+        if obj.obj_user:
+            return obj.obj_user.get_full_name()
+        return ""
+
