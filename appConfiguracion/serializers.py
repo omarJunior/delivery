46a373c01e48f3a5e779cd78ed042c403b199aea
Omar junior <omarhernandez041204@gmail.com>
2022-02-14 11:37:16 -0500

    Cambios en el serializer de configuracion

--- appConfiguracion/serializers.py
+++ appConfiguracion/serializers.py
@@ -5,29 +5,40 @@ class TipoIdentificacionSerializer(serializers.ModelSerializer):
     class Meta:
         model = TipoIndetificacion
         fields = ('__all__')
-        extra_kwargs = {'id': {'required': True}}
 
 class RubroSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rubro
         fields = ('__all__')
-        extra_kwargs = {'id': {'required': True}}
 
 class StockSerializer(serializers.ModelSerializer):
     class Meta:
         model = Stock
         fields = ('__all__')
-        extra_kwargs = {'id': {'required': True}}
 
 class IngredienteSerializer(serializers.ModelSerializer):
+    obj_stock = serializers.SerializerMethodField(method_name="get_stock")
+
     class Meta:
         model = Ingrediente
-        fields = ('__all__')
-        extra_kwargs = {'id': {'required': True}}
+        fields = [
+            'id',
+            'descripcion',
+            'cantidad',
+            'unidad_medida',
+            'costo',
+            'obj_stock'
+        ]
+        read_only_fields = ('obj_stock',) 
+
+    def get_stock(self, obj):
+        if obj.obj_stock:
+            return f"Stock Maximo {obj.obj_stock.stock}, Stock Minimo {obj.obj_stock.stock_minimo}" 
+        return ""
 
 class FormaPagoSerializer(serializers.ModelSerializer):
     class Meta:
         model = FormaPago
         fields = ('__all__')
-        extra_kwargs = {'id': {'required': True}}
+
     
