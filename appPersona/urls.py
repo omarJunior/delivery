2409e4504308f4af3feb59bfb8f216e58b1a915a
Omar junior <omarhernandez041204@gmail.com>
2022-02-24 09:40:16 -0500

    cambios en la url de app Persona

--- appPersona/urls.py
+++ appPersona/urls.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import DefaultRouter
-from .viewsets import ClienteViewSet, PersonaViewSet
+from .viewsets import ClienteViewSet, PersonaViewSet, UserViewSet
 
 router = DefaultRouter()
 router.register(r'cliente', ClienteViewSet)
 router.register(r'persona', PersonaViewSet)
+router.register(r'user', UserViewSet)
