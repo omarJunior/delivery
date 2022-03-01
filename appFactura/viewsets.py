9d92e8f77dc75f5a02e3b35b5c710ea974099944
Omar junior <omarhernandez041204@gmail.com>
2022-03-01 14:48:18 -0500

    cambios en el viewsets de app factura

--- appFactura/viewsets.py
+++ appFactura/viewsets.py
@@ -1,6 +1,7 @@
 from django.http.response import HttpResponse
 from rest_framework import viewsets, status, exceptions
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 from rest_framework.decorators import action
 from appPersona.models import Cliente
 from appConfiguracion.models import FormaPago, Rubro, Stock
@@ -17,7 +18,7 @@ from .serializers import (
 )
 
 class FacturaViewSet(viewsets.ModelViewSet):
-    queryset = Factura.objects.all().order_by('id')
+    queryset = Factura.objects.filter(sys_active = True).order_by('id')
     serializer_class = FacturaSerializer
 
     #cantidad de facturas que existen
@@ -26,7 +27,7 @@ class FacturaViewSet(viewsets.ModelViewSet):
         facturas = Factura.objects.filter(sys_active = True).count()
         return Response({'facturas': facturas})
 
-    @action(detail = False, methods=['post'], url_path="registro-factura", url_name="registro_factura")
+    @action(detail = False, methods=['post'], url_path="register_factura", url_name="register-factura")
     def registro_factura(self, request):
         nro_comprobante = request.data['nro_comprobante']
         fecha = request.data['fecha']
@@ -45,45 +46,87 @@ class FacturaViewSet(viewsets.ModelViewSet):
         factura_obj.descuento = descuento
         factura_obj.save()
 
+    #total money factura
+    @action(detail=False, methods=['get'], url_path="get_total", url_name="get-total")
+    def get_total(self, request):
+        facturas = Factura.objects.all().values('id', 'total')
+        contador = 0
+        for i in facturas:
+            contador = contador + i['total']
+        return Response({'total_money_factura': contador})
+
+
 class ProductoViewSet(viewsets.ModelViewSet):
-    queryset = Producto.objects.all().order_by('id')
+    queryset = Producto.objects.filter(sys_active = True).order_by('id')
     serializer_class = ProductoSerializer
 
-    #Cantidad de productos que existen
+    #cantidad de productos que existen
     @action(detail=False, methods=['get'], url_path="get_productos", name="get-productos")
     def get_productos(self, request):
         productos = Producto.objects.filter(sys_active = True).count()
         return Response({'productos': productos})
 
-    @action(detail=False, methods=['post'], url_path="register-producto", url_name="register_producto")
+    @action(detail=False, methods=['post'], url_path="register_producto", url_name="register-producto")
     def register_producto(self, request):
-        descripcion = request.data['descripcion']
-        costo = request.data['costo']
-        precio_final = request.data['precio_final']
-        id_rubro = request.data['id_rubro']
-        id_stock = request.data['id_stock']
-        tamanio = request.data['tamanio']
-
+        try:
+            descripcion = request.data['descripcion']
+        except:
+            descripcion = None
+        try:
+            costo = request.data['costo'] 
+        except:
+            costo = None
         try:
             precio_sin_iva = request.data['precio_sin_iva']
         except:
             precio_sin_iva = None
-
+        try:
+            precio_final = request.data['precio_final']
+        except:
+            precio_final = None
+        try:
+            id_rubro = request.data['id_rubro']
+        except:
+            id_rubro = None
+        try:
+            id_stock = request.data['id_stock']
+        except:
+            id_stock = None
+        try:
+            tamanio = request.data['tamanio']
+        except:
+            tamanio = None
+       
         producto = Producto()
-        producto.descripcion = descripcion
-        producto.costo = costo
-        producto.precio_final = precio_final
+        if descripcion is not None:
+            producto.descripcion = descripcion
+        if costo is not None:
+            producto.costo = costo
+        if precio_final is not None:
+            producto.precio_final = precio_final
         if precio_sin_iva is not None:
             producto.precio_sin_iva = precio_sin_iva
-            
-        producto.obj_rubro = Rubro()
-        producto.obj_rubro.id = id_rubro
-        producto.obj_stock = Stock()
-        producto.obj_stock.id = id_stock
-        producto.tamanio = tamanio
+        if id_rubro is not None:
+            producto.obj_rubro = Rubro()
+            producto.obj_rubro.id = id_rubro
+        if id_stock is not None:
+            producto.obj_stock = Stock()
+            producto.obj_stock.id = id_stock
+        if tamanio is not None:
+            producto.tamanio = tamanio
         producto.save()
+        return Response({'msg': 'Datos guardados correctamente'})
 
-    
+    #total costo de los productos
+    @action(detail=False, methods=['get'], url_path="get_total_costo", url_name="get-total-costo")
+    def get_total_costo(self, request):
+        productos = Producto.objects.all().values('id', 'costo')
+        contador = 0
+        for i in productos:
+            contador = contador + i['costo']
+        return Response({'total_costo_producto': contador})
+
+
 class RecetaViewSet(viewsets.ModelViewSet):
     queryset = Receta.objects.all().order_by('id')
     serializer_class = RecetaSerializer
