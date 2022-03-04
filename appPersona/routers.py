4e3c615e6f5db9edb1124871e8f672ee714dc277
Omar junior <omarhernandez041204@gmail.com>
2022-03-04 12:13:01 -0500

    routers appPersona

---
+++ appPersona/routers.py
@@ -0,0 +1,9 @@
+from rest_framework.routers import DefaultRouter
+from .viewsets import ClienteViewSet, PersonaViewSet, UserViewSet
+
+router = DefaultRouter()
+router.register(r'cliente', ClienteViewSet)
+router.register(r'persona', PersonaViewSet)
+router.register(r'user', UserViewSet)
+
+urlpatterns = router.urls
